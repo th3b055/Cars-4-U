4e2018c85ddeb751111b86ba65b0f3c213effd1e
github username <github email address>
2015-01-07 17:28:10 -0500

    setting flie structure

--- app.rb
+++ app.rb
@@ -7,4 +7,19 @@ class MyApp < Sinatra::Base
     erb :index
   end
 
+  get '/question1' do
+  	erb :question1
+  end
+
+  get '/question2' do
+  	erb :question2
+  end
+
+  get '/question3' do
+  	erb :question3
+  end
+
+  get '/question4' do
+  	erb :question4
+  end
 end
